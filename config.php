4412c1816f4b627297888254e10240810abbca13
alejoko <alejandroarg@live.com>
2012-09-12 18:16:28 +0300

    acasaus

--- config.php
+++ config.php
@@ -1,20 +1,22 @@
 <?php
 
     define('MAIL_SEND_INFO', 'alejandro.perez@concatel.com');
-    define('MAIL_AUTH',     'alejandro.perez@concatel.com');
+    define('MAIL_AUTH',      'alejandro.perez@concatel.com');
     
-    define('DB_HOSTNAME', 'srv106.concatel.com');
-    define('DB_USERNAME', 'siiuser');
-    define('DB_PASSWORD', 'user00');        
-    define('DB_DATABASE', 'pfwecm');
+    define('DB_HOSTNAME',    'srv106.concatel.com');
+    define('DB_USERNAME',      'siiuser');
+    define('DB_PASSWORD',    'user00');        
+    define('DB_DATABASE',    'pfwecm');
     
-    define( 'PARTNERID',    '277');
-    define( 'USERID',       'AGOUETTA@sii.fr');
+    define( 'PARTNERID',     '277');
+    define( 'USERID',        'AGOUETTA@sii.fr');
     define( 'PASSWORD',      'GOUETTA');
+    define( 'INIDATE',       date("d/m/y 00:00:00"));
+    define( 'ENDDATE',       date("d/m/y 23:59:59"));
     
-    define( 'ENDPOINT', 'http://testadepsep.apec.fr/tools?wsdl');
-    define( 'WSDL_METHODS', 'http://testadepsep.apec.fr/positions?wsdl');
-    define( 'ENCODING', 'UTF-8');
+    define( 'ENDPOINT',      'http://testadepsep.apec.fr/tools?wsdl');
+    define( 'WSDL_METHODS',  'http://testadepsep.apec.fr/positions?wsdl');
+    define( 'ENCODING',      'UTF-8');
     
     define('SOAP_CONNNECTION_ATTEMPS',6);
     define('SECONDS_BETWEEN_ATTEMPS',4);
