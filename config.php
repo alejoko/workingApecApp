d08325bc2669063a5ad8027f6c4ec1b2ea24b428
alejoko <alejandroarg@live.com>
2012-09-13 13:44:54 +0300

    new version (with comments)

--- config.php
+++ config.php
@@ -1,23 +1,28 @@
 <?php
-
+    
     define('MAIL_SEND_INFO', 'alejandro.perez@concatel.com');
-    define('MAIL_AUTH',      'alejandro.perez@concatel.com');
+    define('MAIL_AUTH', 'alejandro.perez@concatel.com');
     
-    define('DB_HOSTNAME',    'srv106.concatel.com');
-    define('DB_USERNAME',      'siiuser');
-    define('DB_PASSWORD',    'user00');        
-    define('DB_DATABASE',    'pfwecm');
+    define('DB_HOSTNAME', 'srv106.concatel.com');
+    define('DB_USERNAME', 'siiuser');
+    define('DB_PASSWORD', 'user00');
+    define('DB_DATABASE', 'pfwecm');
     
-    define( 'PARTNERID',     '277');
-    define( 'USERID',        'AGOUETTA@sii.fr');
-    define( 'PASSWORD',      'GOUETTA');
-    define( 'INIDATE',       date("Y-m-d 00:00:00"));
-    define( 'ENDDATE',       date("Y-m-d 23:59:59"));
+    // Defined TEST values. These values must change in PROD
+    define( 'PARTNERID', '277');
+    define( 'USERID', 'AGOUETTA@sii.fr');
+    define( 'PASSWORD', 'GOUETTA');
     
-    define( 'ENDPOINT',      'http://testadepsep.apec.fr/tools?wsdl');
-    define( 'WSDL_METHODS',  'http://testadepsep.apec.fr/positions?wsdl');
-    define( 'ENCODING',      'iso-8859-1');
+    // Defined default dates (date initial and date end)
+    define( 'INIDATE', date("Y-m-d 00:00:00"));
+    define( 'ENDDATE', date("Y-m-d 23:59:59"));
     
+    // Defined WSDL url TEST values. These values must change in PROD
+    define( 'ENDPOINT', 'http://testadepsep.apec.fr/tools?wsdl');
+    define( 'WSDL_METHODS', 'http://testadepsep.apec.fr/positions?wsdl');
+    define( 'ENCODING', 'iso-8859-1');
+    
+    // Defined number of attempts of connection and number of seconds between attempts
     define('SOAP_CONNNECTION_ATTEMPS',6);
     define('SECONDS_BETWEEN_ATTEMPS',4);
     
